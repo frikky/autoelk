53b849b27fa06d2ed7a49f7494479584796195c7
frikky <frikky@dev.wsg.int>
2017-09-20 19:15:19 +0100

    Added comment and ipstuff

--- elk.sh
+++ elk.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+ip=$(ip a | grep -E "eth0|ens160" | grep inet | grep -Po "\d+\.\d+\.\d+\.\d+" | head -1)
 git clone https://github.com/deviantony/docker-elk
 cd docker-elk
 docker-compose up
+echo "\n[!] ELK setup finished. Kibana is up at $ip:5601 in 2~ minutes."
