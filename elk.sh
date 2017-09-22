a8aeae71934e8b66b0f4f24a324984dc4feee458
frikky <frikky@dev.wsg.int>
2017-09-22 12:37:49 +0100

    Docker-compose daemonizing

--- elk.sh
+++ elk.sh
@@ -3,5 +3,5 @@
 ip=$(ip a | grep -E "eth0|ens160" | grep inet | grep -Po "\d+\.\d+\.\d+\.\d+" | head -1)
 git clone https://github.com/deviantony/docker-elk
 cd docker-elk
-docker-compose up
+docker-compose up -d
 echo "\n[!] ELK setup finished. Kibana is up at $ip:5601 in 2~ minutes."
